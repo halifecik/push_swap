36293432234dc7bf6ea239ac93b356de3bc0eb00
Halil Demirci <halife@Halils-MacBook-Air.local>
2025-04-07 18:48:59 +0300

    Updated utils library.

--- utils/utils.h
+++ utils/utils.h
@@ -1,7 +1,20 @@
 #ifndef UTILS_H
-#define UTILS_H
+# define UTILS_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+void	*ft_memset(void *s, int c, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+char	**ft_split(char const *s, char c);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
