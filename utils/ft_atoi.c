dadbabed9884a62a62ad0eeee09fd55a75c9681e
Halil Demirci <halife@Halils-MacBook-Air.local>
2025-04-08 17:09:41 +0300

    Optimized ft_atoi.

--- utils/ft_atoi.c
+++ utils/ft_atoi.c
@@ -1,22 +1,20 @@
 #include "utils.h"
 
-int	ft_atoi(const char *str)
+long	ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	result;
+	long	i;
+	long	sign;
+	long	result;
 
 	result = 0;
 	sign = 1;
 	i = 0;
 	while ((str[i] == 32) || (str[i] >= 9 && str[i] <= 13))
 		i++;
+	if (str[i] == 45)
+		sign = -1;
 	if ((str[i] == 45) || (str[i] == 43))
-	{
-		if (str[i] == 45)
-			sign = -1;
 		i++;
-	}
 	while (str[i] >= 48 && str[i] <= 57)
 		result = result * 10 + (str[i++] - 48);
 	return (result * sign);
